aa3282d87b1e82a10ae311cecfdfb2a51c348cc9
aidowu1 <adeidowu@hotmail.com>
2023-10-28 15:16:39 +0100

    Minor changes to the Azure CLI script

--- Airflow/Run-an-existing-pipeline-with-Managed-Airflow/stepup_scripts.sh
+++ Airflow/Run-an-existing-pipeline-with-Managed-Airflow/stepup_scripts.sh
@@ -5,25 +5,25 @@ LOCATION = "uksouth"
 STORAGE_ACCOUNT_NAME = airflow-dags
 EMAIL = "adeidowu@hotmail.com"
 CONTAINER_NAME = "airflow-dags"
-ROLE = "/subscriptions//resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Storage/storageAccounts/$STORAGE_ACCOUNT_NAME/blobServices/default/containers/$CONTAINER_NAME"
+ROLE = "/subscriptions//resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Storage/storageAccounts/${STORAGE_ACCOUNT_NAME}/blobServices/default/containers/${CONTAINER_NAME}"
 
 
 # Create Resource group
-az group create --name $RESOURCE_GROUP --location $LOCATION
+az group create --name ${RESOURCE_GROUP} --location ${LOCATION}
 
 # Create Storage Account
 az storage account create \
-    --name $STORAGE_ACCOUNT_NAME \
-    --resource-group $RESOURCE_GROUP \
-    --location $LOCATION \
+    --name ${STORAGE_ACCOUNT_NAME} \
+    --resource-group ${RESOURCE_GROUP} \
+    --location ${LOCATION} \
     --dns-endpoint-type AzureDnsZone
 
 az storage container create \
-    --account-name $STORAGE_ACCOUNT_NAME \
-    --name $CONTAINER_NAME \
+    --account-name ${STORAGE_ACCOUNT_NAME} \
+    --name ${CONTAINER_NAME} \
     --auth-mode login
 
 # Create the write role
 az role assignment create --role "Storage Blob Data Contributor" \
-   --assignee $EMAIL \ 
-   --scope $ROLE
+   --assignee ${EMAIL} \ 
+   --scope ${ROLE}
